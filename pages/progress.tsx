8c04fb6570de402d89e6107a0fe6452d2bbbe1d3
an0304073@naver.com <an0304073@naver.com>
2021-02-11 18:35:50 +0900

    react progress

---
+++ pages/progress.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Progress } from "react-sweet-progress";
+import "react-sweet-progress/lib/style.css";
+
+const progress = () => {
+  return <Progress percent={88} />;
+};
+
+export default progress;
